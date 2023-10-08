c6a239b2dd8ed5171fb93d356e51c2d702eaefa3
dhgustjr <144310753+dhgustjr@users.noreply.github.com>
2023-10-08 13:55:43 +0900

    [소프트웨어학과 오현석]
    몬테 카를로 알고리즘 과제 제출하겠습니다.

---
+++ 23.10.8 c 프로과제/23.10.8 cpro .cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define TOTAL_REPEATS 1000000000
+#define PRINT_INTERVAL 10000000
+
+int main() {
+    double x, y; // x, y ÁÂÇ¥
+    long long count = 0, inside_circle = 0;
+
+    srand(time(NULL));
+
+    for (count = 1; count <= TOTAL_REPEATS; count++) {
+        x = (double)rand() / (double)RAND_MAX;
+        y = (double)rand() / (double)RAND_MAX;
+
+        if (x * x + y * y <= 1.0) {
+            inside_circle++;
+        }
+
+        if (count % PRINT_INTERVAL == 0) {
+            double pi_estimate = 4.0 * inside_circle / count;
+            printf("After %lld repeats, estimated pi = %f\n", count, pi_estimate);
+        }
+    }
+
+    double final_pi_estimate = 4.0 * inside_circle / TOTAL_REPEATS;
+    printf("Final estimated value of pi after %lld repeats: %f\n", TOTAL_REPEATS, final_pi_estimate);
+
+    return 0;
+}
