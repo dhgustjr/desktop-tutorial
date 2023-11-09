33903048ee8c202fdc36a5c8f732dd8c1774a143
dhgustjr <144310753+dhgustjr@users.noreply.github.com>
2023-11-09 21:17:10 +0900

    Update 23.11.9 실습2번.cpp

--- 23.11.9 실습/23.11.9 실습2번.cpp
+++ 23.11.9 실습/23.11.9 실습2번.cpp
@@ -3,15 +3,11 @@
 int main() {
     int arr[5][5];
     int num = 1;
-
-    // 배열에 숫자 채우기
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
             arr[i][j] = num++;
         }
     }
-
-    // 배열 출력하기
     printf("원래 배열:\n");
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
@@ -19,8 +15,6 @@ int main() {
         }
         printf("\n");
     }
-
-    // 행과 열을 바꾸어 출력하기
     printf("\Change row & cloumn \n");
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
